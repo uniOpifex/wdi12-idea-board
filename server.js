561c3dc65b224bdf628ebd106e3be6510f354db9
king0120 <king0120@gmail.com>
2017-10-12 11:18:50 -0400

    added create-react-app

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 })
 
 // Set the app to listen on a specific port
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log('App listening on port: ', PORT)
 })
